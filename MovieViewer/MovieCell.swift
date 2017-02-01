4ea8b2a1d3b989b06aa967c04d9508332c20de7c
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-01-31 19:33:41 -0800

    Finished the Setup Section of the Assignment
    1. Creates a TableView with a list of movies with titles and overview
    2. Makes API call to fetch these movies

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Sandeep Raghunandhan on 1/31/17.
+//  Copyright © 2017 Sandeep Raghunandhan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var overview: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
